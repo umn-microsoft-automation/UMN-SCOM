5b3fccacfa95f1416d5d61a484085c237cb8dd02
skc <skc@umn.edu>
2019-02-22 15:14:47 -0600

    Initial Release
    Initial Release

---
+++ README.md
@@ -0,0 +1,7 @@
+# UMN-SCOM
+
+## Update '1.0.0'
+
+This is the initial release.
+
+The Function Restart-SCOMHealth is meant to stop the SCOM agent service delete the health log files then start the service which repopulates the log and should get new hearbeat.
---
+++ UMN-SCOM.psm1
@@ -0,0 +1,50 @@
+﻿    ###
+# Copyright 2017 University of Minnesota, Office of Information Technology
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+###
+
+#region Dependancies
+Function Restart-SCOMHealth
+{
+    param(
+    [Parameter(madatory=$true)]$HostName
+    )
+    [cmdletbinding]
+    try
+    {
+        Get-Service -Name "HealthService" -ComputerName $HostName | Stop-Service
+        Start-Sleep -Seconds 5
+        if((Get-Service -Name "HealthService" -ComputerName $HostName).Status -ne "Running")
+        {
+            Invoke-Command -ComputerName $HostName -ScriptBlock {Remove-Item -Path "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Health Service Store\" -Recurse -Force} 
+            Start-Sleep -Seconds 5
+        }
+        Get-Service -Name "HealthService" -ComputerName $HostName | Start-Service
+    }
+    catch
+    {
+        if((Get-Service -Name "HealthService" -ComputerName $HostName).Status -ne "Running")
+        {
+            Write-Verbose "Error Running Script Occured!, Service is not running, Check Manually"
+        }
+        else 
+        {
+            Write-Verbose "Error Running Script Occured!"
+        }
+        
+    }
+}
+
+Export-ModuleMember -Function *
